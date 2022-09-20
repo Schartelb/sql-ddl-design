044fcdaad26642ea10cc5af9cae884b34b620dad
Schartelb <106553010+Schartelb@users.noreply.github.com>
2022-09-20 08:59:02 -0400

    Add files via upload

--- music.sql
+++ music.sql
@@ -7,27 +7,65 @@ CREATE DATABASE music;
 
 \c music
 
+CREATE TABLE artist
+(
+  id SERIAL PRIMARY KEY,
+  name TEXT UNIQUE NOT NULL
+);
+
+CREATE TABLE producer
+(
+  id SERIAL PRIMARY KEY,
+  name TEXT UNIQUE NOT NULL
+);
+
 CREATE TABLE songs
 (
   id SERIAL PRIMARY KEY,
   title TEXT NOT NULL,
   duration_in_seconds INTEGER NOT NULL,
   release_date DATE NOT NULL,
-  artists TEXT[] NOT NULL,
-  album TEXT NOT NULL,
-  producers TEXT[] NOT NULL
+  artists INTEGER REFERENCES artist,
+  producers INTEGER REFERENCES producer
+);
+
+CREATE TABLE album
+(
+  id SERIAL PRIMARY KEY,
+  title TEXT NOT NULL,
+  song_id INTEGER NOT NULL UNIQUE REFERENCES songs
 );
 
 INSERT INTO songs
-  (title, duration_in_seconds, release_date, artists, album, producers)
+  (title, duration_in_seconds, release_date, artists, producers)
 VALUES
-  ('MMMBop', 238, '04-15-1997', '{"Hanson"}', 'Middle of Nowhere', '{"Dust Brothers", "Stephen Lironi"}'),
-  ('Bohemian Rhapsody', 355, '10-31-1975', '{"Queen"}', 'A Night at the Opera', '{"Roy Thomas Baker"}'),
+  ('MMMBop', 238, '04-15-1997', 1, 'Middle of Nowhere', '{"Dust Brothers", "Stephen Lironi"}'),
+  ('Bohemian Rhapsody', 355, '10-31-1975', 2, 'A Night at the Opera', '{"Roy Thomas Baker"}'),
   ('One Sweet Day', 282, '11-14-1995', '{"Mariah Cary", "Boyz II Men"}', 'Daydream', '{"Walter Afanasieff"}'),
   ('Shallow', 216, '09-27-2018', '{"Lady Gaga", "Bradley Cooper"}', 'A Star Is Born', '{"Benjamin Rice"}'),
-  ('How You Remind Me', 223, '08-21-2001', '{"Nickelback"}', 'Silver Side Up', '{"Rick Parashar"}'),
+  ('How You Remind Me', 223, '08-21-2001',7, 'Silver Side Up', '{"Rick Parashar"}'),
   ('New York State of Mind', 276, '10-20-2009', '{"Jay Z", "Alicia Keys"}', 'The Blueprint 3', '{"Al Shux"}'),
   ('Dark Horse', 215, '12-17-2013', '{"Katy Perry", "Juicy J"}', 'Prism', '{"Max Martin", "Cirkut"}'),
   ('Moves Like Jagger', 201, '06-21-2011', '{"Maroon 5", "Christina Aguilera"}', 'Hands All Over', '{"Shellback", "Benny Blanco"}'),
-  ('Complicated', 244, '05-14-2002', '{"Avril Lavigne"}', 'Let Go', '{"The Matrix"}'),
-  ('Say My Name', 240, '11-07-1999', '{"Destiny''s Child"}', 'The Writing''s on the Wall', '{"Darkchild"}');
+  ('Complicated', 244, '05-14-2002', 14, 'Let Go', '{"The Matrix"}'),
+  ('Say My Name', 240, '11-07-1999', 15, 'The Writing''s on the Wall', '{"Darkchild"}');
+
+INSERT INTO artist
+(name)
+VALUES
+('Hanson'),
+('Queen'),
+('Mariah Carey'),
+('Boyz II Men'),
+('Lady Gaga'),
+('Bradley Cooper'),
+('Nickelback'),
+('Jay Z'),
+('Alicia Keys'),
+('Katy Perry'),
+('Juicy J'),
+('Maroon 5'),
+('Christina Aguilera'),
+('Avril Lavigne'),
+('Destiny''s Child')
+;
